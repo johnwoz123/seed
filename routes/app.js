521adbf4dc740045dd4c3ed900691588931f19ab
John Wozniak <jjwozn0977@gmail.com>
2017-02-23 16:13:06 -0500

    front end routing editing
    Starting to get a flow for the front end routing

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,16 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+// the :msg is used to set up to be able to recieve the post from POSt message
+router.get('/message/:msg', function(req, res, next) {
+  // res.render('node', {message: 'hello!'});
+  res.render('node', {message: req.params.msg});
+});
+
+router.post('/message', function(req,res,next) {
+  var message = req.body.message;
+  //redirect to only get routes
+  res.redirect('/message/' + message)
+})
+
 module.exports = router;
